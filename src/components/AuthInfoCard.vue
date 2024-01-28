2adf9c497f98e04b7f5686fb8946b8d38b58d892
sofie <sophiess@seznam.cz>
2024-01-28 14:02:19 +0100

    - auth info card

---
+++ src/components/AuthInfoCard.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-card v-if="expiresAt" variant="outlined">
+    <v-card-title>State flags</v-card-title>
+    <v-card-text>
+      <div>Expires at: {{ expiresAt }}</div></v-card-text
+    ></v-card
+  >
+</template>
+
+<script lang="ts" setup>
+defineProps({
+  expiresAt: String,
+});
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss"></style>
--- src/components/DomainDetail.vue
+++ src/components/DomainDetail.vue
@@ -1,10 +1,18 @@
 <template>
   <div v-if="domainData" class="hello">
-    <EventsCard :events="domainData.events"></EventsCard>
-    <KeySetCard :key-set="domainData.keyset"></KeySetCard>
-    <NSSetCard :ns-set="domainData.nsset"></NSSetCard>
-    <OwnerCard :owner="domainData.owner"></OwnerCard>
-    <StateFlagsCard :state-flags="domainData.state_flags"></StateFlagsCard>
+    {{ domainData.fqdn }}
+    <v-row>
+      <v-col>
+        <AuthInfoCard :expires-at="domainData.expires_at"></AuthInfoCard>
+        <EventsCard :events="domainData.events"></EventsCard>
+        <StateFlagsCard :state-flags="domainData.state_flags"></StateFlagsCard
+      ></v-col>
+      <v-col>
+        <OwnerCard :owner="domainData.owner"></OwnerCard>
+        <KeySetCard :key-set="domainData.keyset"></KeySetCard>
+        <NSSetCard :ns-set="domainData.nsset"></NSSetCard>
+      </v-col>
+    </v-row>
   </div>
 </template>
 
@@ -17,6 +25,7 @@ import KeySetCard from "@/components/KeySetCard.vue";
 import NSSetCard from "@/components/NSSetCard.vue";
 import OwnerCard from "@/components/OwnerCard.vue";
 import StateFlagsCard from "@/components/StateFlagsCard.vue";
+import AuthInfoCard from "@/components/AuthInfoCard.vue";
 
 const domainData = ref<DomainTo | null>(null);
 
